59d9bf0cce6823bf654304f09c5e77f5820700cf
Elliot Davies <elliot.a.davies@gmail.com>
2018-08-09 19:08:26 +0100

    Eject create-elm-app and restructure directory

---
+++ wp-content/plugins/jr_joblistings/public/js/index.js
@@ -0,0 +1,7 @@
+import '../css/main.css';
+import { Main } from '../elm/src/Main.elm';
+// import registerServiceWorker from './registerServiceWorker';
+
+Main.embed(document.getElementById('elm'));
+
+// registerServiceWorker();
--- wp-content/plugins/jr_joblistings/public/partials/jr-joblistings-public-display.php
+++ wp-content/plugins/jr_joblistings/public/partials/jr-joblistings-public-display.php
@@ -14,3 +14,5 @@
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
 <h1>JR Job Listings</h1>
+
+<div id="elm"></div>
