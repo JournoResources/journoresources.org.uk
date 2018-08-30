bebe1ec53e03273cc9bce012cf6975c5d78f40e4
Elliot Davies <elliot.a.davies@gmail.com>
2018-08-30 19:10:27 +0100

    Single page styling

--- wp-content/plugins/jr_joblistings/public/partials/default-jr_joblistings-single.php
+++ wp-content/plugins/jr_joblistings/public/partials/default-jr_joblistings-single.php
@@ -14,12 +14,17 @@
   $listing_url = get_field('listing_url');
   $logo = get_field('company_logo');
   $description = get_field('job_description_full');
+
+  $formattedDate = date( 'd/m/Y', strtotime( $expiry_date ) );
+  $formattedToday = date( 'd/m/Y' );
+
+  $isToday = $formattedDate == $formattedToday;
 ?>
 
 <div id="primary">
   <div id="content">
     <section class="jr_joblisting type-jr_joblisting">
-      <article>
+      <article id="jr-single-job">
         <header class="entry-header">
           
           <h2 class="entry-title jr-job-title"><?php echo $title . ', ' . $employer; ?></h2>
@@ -28,7 +33,7 @@
             <div class="by-author vcard author">
               <span class="fn">
               <a href="<?php echo get_author_posts_url( $authorID ); ?>">
-                <?php echo get_author_name($authorID); ?>
+                Posted by <?php echo get_author_name($authorID); ?>
               </a>
               </span>
             </div>
@@ -45,9 +50,24 @@
     
         <div class="entry-content clearfix">
           
-          <div class="jr-job-description"><?php echo $location . ' - ' . $salary; ?></div>
+          <div class="jr-job-location">
+            <strong>Location: </strong> <?php echo $location ?>
+          </div>
+
+          <div class="jr-job-salary">
+            <strong>Salary details: </strong> <?php echo $salary ?>
+          </div>
           
-          <div class="jr-job-expiry"><?php echo 'Closes ' . $expiry_date; ?></div>
+          <div class="jr-job-expiry">
+            <?php
+            if ($isToday) {
+              echo '<strong>Closes today</strong>';
+            } else {
+              echo '<strong>Closes: </strong>' . $formattedDate;
+            } ?>
+          </div>
+
+          <hr class="jr-divider" />
           
           <div class="jr-company-logo">
             <img src="<?php echo $logo; ?>" alt="<?php echo $employer; ?>" />
