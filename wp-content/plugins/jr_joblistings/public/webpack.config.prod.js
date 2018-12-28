51aca7695a7177da9ae7216aeffb3acc09f6b10e
Elliot Davies <elliot.a.davies@gmail.com>
2018-12-28 11:16:06 +0000

    Webpack build process

---
+++ wp-content/plugins/jr_joblistings/public/webpack.config.prod.js
@@ -0,0 +1,60 @@
+"use strict";
+
+const path = require("path");
+const webpack = require("webpack");
+
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
+const buildPath = __dirname + "/build";
+
+module.exports = {
+  entry: "./index.js",
+
+  output: {
+    path: buildPath,
+    pathinfo: false
+  },
+
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true
+      }),
+
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+
+  plugins: [new CleanWebpackPlugin([buildPath]), new MiniCssExtractPlugin({})],
+
+  module: {
+    rules: [
+      {
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        use: [
+          {
+            loader: "elm-webpack-loader",
+            options: {
+              optimize: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"]
+      }
+    ]
+  },
+
+  resolve: {
+    modules: ["node_modules"],
+    extensions: [".js", ".elm"]
+  }
+};
