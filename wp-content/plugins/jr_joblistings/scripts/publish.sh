8885291b759db126933ae6199495cd02109dbe3e
Elliot Davies <elliot.a.davies@gmail.com>
2019-07-01 21:58:59 +0100

    Initial version of publish script

---
+++ wp-content/plugins/jr_joblistings/scripts/publish.sh
@@ -0,0 +1,8 @@
+set -x
+
+version=$(jq .version public/package.json | tr -d '"')
+target=$1
+
+sed -i '' "s/${version}/${target}/g" public/package.json
+sed -i '' "s/${version}/${target}/g" Makefile
+sed -i '' "s/${version}/${target}/g" jr_joblistings.php
