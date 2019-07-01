6feef1bf9adc665f96af1f13eac4dcbc0dc917ae
Elliot Davies <elliot.a.davies@gmail.com>
2019-07-01 22:18:03 +0100

    Improve publishing workflow (#47)
    Add `make publish` command and refactor `build` process to use Bash script

---
+++ wp-content/plugins/jr_joblistings/scripts/build.sh
@@ -0,0 +1,19 @@
+set -x
+
+version=$(jq .version public/package.json | tr -d '"')
+
+mkdir -p "jr-joblistings-v${version}/public"
+
+pushd public
+yarn build
+popd
+
+cp -R public/build public/class-jr-joblistings-public.php public/index.php public/partials \
+  "jr-joblistings-v${version}/public"
+
+cp -R admin includes uninstall.php index.php vendor jr_joblistings.php languages \
+  "jr-joblistings-v${version}"
+
+zip -r "jr-joblistings-v${version}.zip" "jr-joblistings-v${version}"
+
+rm -r "jr-joblistings-v${version}"
---
+++ wp-content/plugins/jr_joblistings/scripts/publish.sh
@@ -0,0 +1,7 @@
+set -x
+
+version=$(jq .version public/package.json | tr -d '"')
+target=$1
+
+sed -i '' "s/${version}/${target}/g" public/package.json
+sed -i '' "s/${version}/${target}/g" jr_joblistings.php
