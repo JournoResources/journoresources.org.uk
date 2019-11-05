e29135c70780ff5f091e44405463daf76ab241c0
Elliot Davies <elliot.a.davies@gmail.com>
2019-11-05 22:41:27 +0000

    Make logo and full-desc fields optional

--- wp-content/plugins/jr_joblistings/admin/class-jr-joblistings-admin.php
+++ wp-content/plugins/jr_joblistings/admin/class-jr-joblistings-admin.php
@@ -383,19 +383,8 @@ class JR_JobListings_Admin {
 						'label' => 'Job description (full)',
 						'name' => 'job_description_full',
 						'type' => 'wysiwyg',
-						'instructions' => 'This description will appear on the individual job page',
-						'required' => 1,
-						/* 'conditional_logic' => array ( */
-						/* 	'status' => 1, */
-						/* 	'rules' => array ( */
-						/* 		array ( */
-						/* 			'field' => 'field_5b71c19a7ae3c', */
-						/* 			'operator' => '==', */
-						/* 			'value' => '1', */
-						/* 		), */
-						/* 	), */
-						/* 	'allorany' => 'all', */
-						/* ), */
+						'instructions' => 'This description will appear on the individual job page. Required for promoted jobs',
+						'required' => 0,
 						'default_value' => '',
 						'toolbar' => 'full',
 						'media_upload' => 'no',
@@ -406,19 +395,8 @@ class JR_JobListings_Admin {
 						'label' => 'Company logo',
 						'name' => 'company_logo',
 						'type' => 'image',
-						'instructions' => 'The logo of the company advertising this job',
-						'required' => 1,
-						/* 'conditional_logic' => array ( */
-						/* 	'status' => 1, */
-						/* 	'rules' => array ( */
-						/* 		array ( */
-						/* 			'field' => 'field_5b71c19a7ae3c', */
-						/* 			'operator' => '==', */
-						/* 			'value' => '1', */
-						/* 		), */
-						/* 	), */
-							/* 'allorany' => 'all', */
-						/* ), */
+						'instructions' => 'The logo of the company advertising this job, Required for promoted jobs',
+						'required' => 0,
 						'save_format' => 'url',
 						'preview_size' => 'thumbnail',
 						'library' => 'all',
